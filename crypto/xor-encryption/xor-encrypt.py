16aca668ee8d523994882e4c0e0c0bacaf38ea98
Shreethaar <shreethaara380@gmail.com>
2024-05-31 13:43:11 +0000

    remove rsa and add xor

---
+++ crypto/xor-encryption/xor-encrypt.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python3
+
+def encryptDecrypt(input):
+	key = ['C', 'T', 'F']
+	output = []
+	
+	for i in range(len(input)):
+		xor_num = ord(input[i]) ^ ord(key[i % len(key)])
+		output.append(chr(xor_num))
+	
+	return ''.join(output)
+
+
+def main():
+	encrypted = encryptDecrypt("CTF{xor-encrypt-decrypt");
+	print("Encrypted:"+encrypted);
+	
+	decrypted = encryptDecrypt(encrypted);
+	print("Decrypted:"+decrypted);
+	pass
+
+
+if __name__ == '__main__':
+	main()
