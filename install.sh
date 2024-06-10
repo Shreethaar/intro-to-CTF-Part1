a7f167d448d8b0065b920fe092d5759478be4736
Shreethaar <shreethaara380@gmail.com>
2024-06-10 12:17:49 +0000

    web-chall-9-dockering

--- install.sh
+++ install.sh
@@ -11,9 +11,7 @@ sudo apt-get upgrade -y
 sudo apt-get install -y exiftool
 sudo apt-get install -y steghide
 sudo apt-get install -y binwalk
-sudo apt-get install -y python3 python3-pip git
-git clone https://github.com/sherlock-project/sherlock.git
-cd sherlock || exit
-python3 -m pip install -r requirements.txt
-echo 'export PATH=$PATH:~/sherlock' >> ~/.zshrc
-source ~/.zshrc
+sudo apt-get install -y sherlock
+sudo apt-get install -y gedit
+sudo apt-get install -y ffzf
+
